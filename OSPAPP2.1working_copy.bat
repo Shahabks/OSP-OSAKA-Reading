ae1ddb62e6e0c7bcec0fb27d493a79a8b320526a
Shahab Sabahi <27753966+Shahabks@users.noreply.github.com>
2019-02-15 21:22:41 +0900

    Add files via upload

---
+++ OSPAPP2.1working_copy.bat
@@ -0,0 +1,15 @@
+echo off
+COLOR 9E
+echo START 
+echo off
+"C:\Users\Sabahi.s\Desktop\corpus/Praat.exe" --run "C:\Users\Sabahi.s\Desktop\Osaka Project/OSPAPP2.1working_copy.praat" -25 2 0.3 yes "C:\Users\Sabahi.s\Desktop\corpus/one.wav" "C:\Users\Sabahi.s\Desktop\corpus" 80 400 0.01   
+SET n=HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+echo %n% 
+echo off
+echo %n%
+echo THE ANALYSIS IS COMPLETE 
+timeout 3 > NUL
+echo %n%
+echo %n%
+echo GET the RESULTS REPORTS
+pause
